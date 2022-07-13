d583d514b852da31d172dcb5b9b994e763c32cfd
jalarrosap <jalarrosap@eservicios.indra.es>
2022-07-13 19:02:41 +0200

    Subimos...

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Sony', 600, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('LG', 500, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Philips', 400, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Samsung', 300, NOW());

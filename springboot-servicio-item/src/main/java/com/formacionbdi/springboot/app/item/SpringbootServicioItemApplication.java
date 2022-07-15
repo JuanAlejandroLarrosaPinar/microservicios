30da1872a9a4dac0fb23f4cb3597699afdc2604f
jalarrosap <jalarrosap@eservicios.indra.es>
2022-07-15 17:13:43 +0200

    Se implementa servidor eureka habilitando balanceo de carga de eureka y no ribbon

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,12 +2,14 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@RibbonClient("servicio-productos")
+//Lo comentamos para utilizar el balanceo de carga de eureka.
+//@RibbonClient("servicio-productos")
 @SpringBootApplication
 @EnableFeignClients //con esta anotación utilizamos la alternativa a RestTemplate, que es Feign
+@EnableEurekaClient
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {

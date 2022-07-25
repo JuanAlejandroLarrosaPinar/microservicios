1a13c694c22e5932c62500f609960f42c09fda93
JuanAlejandroLarrosaPinar <alejandro.larrosa.pinar@gmail.com>
2022-07-25 18:26:23 +0200

    Implementando GatewayFilterFactory...

--- springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -26,10 +26,10 @@ public class EjemploGlobalFilter implements GlobalFilter, Ordered{
 			//aquí va el post
 			log.info("ejecutando filtro post");
 			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor->{
-				exchange.getResponse().getHeaders().add("token", valor);
+				//exchange.getResponse().getHeaders().add("token", valor);
 			});;
-			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
-			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			//exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
+			//exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
 	}
 	

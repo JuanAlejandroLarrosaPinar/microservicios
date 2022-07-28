3c9ddc8f784de79c519ac79c6990320ea3196088
jalarrosap <jalarrosap@eservicios.indra.es>
2022-07-28 08:54:06 +0200

    Personalizando parámetro del Circuit Breaker

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,9 +1,18 @@
 package com.formacionbdi.springboot.app.item;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JAutoConfiguration;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 //Esta clase la vamos a utilizar para los beans con @Configuration
 @Configuration
@@ -14,4 +23,20 @@ public class AppConfig {
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id->{
+			return new Resilience4JConfigBuilder(id)
+					.circuitBreakerConfig(CircuitBreakerConfig.custom()
+					.slidingWindowSize(10) //cada diez peticiones mal
+					.failureRateThreshold(50) //si el 50% se produce un error, entonces abre el circuito para que este servicio no responda
+					.waitDurationInOpenState(Duration.ofSeconds(10L))
+					.build()).timeLimiterConfig(TimeLimiterConfig.ofDefaults()).build();
+		});
+		
+		
+	}
+	
+	
 }
